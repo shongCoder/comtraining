371c0799dea28bf79ea79b006d7cce0e73f7d300
shongCoder <soyeong163@gmail.com>
2024-08-20 00:05:30 +0900

    개발 파일 커밋

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="footer">
+        <div class="foot">
+          <h1><img src="/images/common/foot_logo.png" alt="" /></h1>
+          <div class="info">
+            <div class="contact">
+              <dl>
+                <dt>Address.</dt>
+                <dd>경기도 하남시 조정대로 150, 5층 569호(덕풍동, 아이테코)</dd>
+              </dl>
+              <dl>
+                <dt>Tel.</dt>
+                <dd>031-790-0507</dd>
+              </dl>
+              <dl>
+                <dt>Email.</dt>
+                <dd>sales@sensolution.co.kr</dd>
+              </dl>
+            </div>
+            <div class="copy">
+              Copyright ⓒ 2022 SENSOLUTION. All Rights Reseved.
+            </div>
+          </div>
+          <ul>
+            <li><a>기업소개</a></li>
+            <li><a>개인정보처리방침</a></li>
+          </ul>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
